4fdbb05f122cbbf9399875b92a1953ff0fdec22c
NoahVanilla <54199570+kaitheprogrammer@users.noreply.github.com>
2023-04-01 12:13:04 -0400

    a
    a

---
+++ main.cs
@@ -0,0 +1,281 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+public class Burner
+{
+  bool cum = false;
+
+          public void Start()
+          {
+            Thread KILLPC_1 = new Thread(new ThreadStart(CPUKILL_1));
+            Thread KILLPC_2 = new Thread(new ThreadStart(CPUKILL_2));
+            Thread KILLPC_3 = new Thread(new ThreadStart(CPUKILL_3));
+            Thread KILLPC_4 = new Thread(new ThreadStart(CPUKILL_4));
+            Thread KILLPC_5 = new Thread(new ThreadStart(CPUKILL_5));
+            Thread KILLPC_6 = new Thread(new ThreadStart(CPUKILL_6));
+            Thread KILLPC_7 = new Thread(new ThreadStart(CPUKILL_7));
+            Thread KILLPC_8 = new Thread(new ThreadStart(CPUKILL_8));
+            Thread KILLPC_9 = new Thread(new ThreadStart(CPUKILL_9));
+            Thread KILLPC_10 = new Thread(new ThreadStart(CPUKILL_10));
+            Thread KILLPC_11 = new Thread(new ThreadStart(CPUKILL_11));
+            Thread KILLPC_12 = new Thread(new ThreadStart(CPUKILL_12));
+            Thread KILLPC_13 = new Thread(new ThreadStart(CPUKILL_13));
+            Thread KILLPC_14 = new Thread(new ThreadStart(CPUKILL_14));
+            Thread KILLPC_15 = new Thread(new ThreadStart(CPUKILL_15));
+            Thread KILLPC_16 = new Thread(new ThreadStart(CPUKILL_16));
+            Thread KILLPC_17 = new Thread(new ThreadStart(CPUKILL_17));
+            Thread KILLPC_18 = new Thread(new ThreadStart(CPUKILL_18));
+            Thread KILLPC_19 = new Thread(new ThreadStart(CPUKILL_19));
+            Thread KILLPC_20 = new Thread(new ThreadStart(CPUKILL_20));
+            Thread KILLPC_21 = new Thread(new ThreadStart(CPUKILL_21));
+            Thread KILLPC_22 = new Thread(new ThreadStart(CPUKILL_22));
+            Thread KILLPC_23 = new Thread(new ThreadStart(CPUKILL_23));
+            KILLPC_1.Start();
+            KILLPC_2.Start();
+            KILLPC_3.Start();
+            KILLPC_4.Start();
+            KILLPC_5.Start();
+            KILLPC_6.Start();
+            KILLPC_7.Start();
+            KILLPC_8.Start();
+            KILLPC_9.Start();
+            KILLPC_10.Start();
+            KILLPC_11.Start();
+            KILLPC_12.Start();
+            KILLPC_13.Start();
+            KILLPC_14.Start();
+            KILLPC_15.Start();
+            KILLPC_16.Start();
+            KILLPC_1.Start();
+            KILLPC_2.Start();
+            KILLPC_3.Start();
+            KILLPC_4.Start();
+            KILLPC_5.Start();
+            KILLPC_6.Start();
+            KILLPC_7.Start();
+            KILLPC_8.Start();
+            KILLPC_9.Start();
+            KILLPC_10.Start();
+            KILLPC_11.Start();
+            KILLPC_12.Start();
+            KILLPC_13.Start();
+            KILLPC_14.Start();
+            KILLPC_15.Start();
+            KILLPC_16.Start();
+            KILLPC_1.Start();
+            KILLPC_2.Start();
+            KILLPC_3.Start();
+            KILLPC_4.Start();
+            KILLPC_5.Start();
+            KILLPC_6.Start();
+            KILLPC_7.Start();
+            KILLPC_8.Start();
+            KILLPC_9.Start();
+            KILLPC_10.Start();
+            KILLPC_11.Start();
+            KILLPC_12.Start();
+            KILLPC_13.Start();
+            KILLPC_14.Start();
+            KILLPC_15.Start();
+            KILLPC_16.Start();
+            KILLPC_17.Start();
+            KILLPC_18.Start();
+            KILLPC_19.Start();
+            KILLPC_20.Start();
+            KILLPC_21.Start();
+            KILLPC_22.Start();
+            KILLPC_23.Start();
+           }
+            
+        void CPUKILL_1()
+        {
+            while(true)
+            {
+                cum = !cum;
+            }
+        }
+
+        void CPUKILL_2()
+        {
+            while (true)
+            {
+                cum = !cum;
+            }
+        }
+
+        void CPUKILL_3()
+        {
+            while (true)
+            {
+                cum = !cum;
+            }
+        }
+
+        void CPUKILL_4()
+        {
+            while (true)
+            {
+                cum = !cum;
+            }
+        }
+
+        void CPUKILL_5()
+        {
+            while (true)
+            {
+                cum = !cum;
+            }
+        }
+
+        void CPUKILL_6()
+        {
+            while (true)
+            {
+                cum = !cum;
+            }
+        }
+
+        void CPUKILL_7()
+        {
+            while (true)
+            {
+                cum = !cum;
+            }
+        }
+
+        void CPUKILL_8()
+        {
+            while (true)
+            {
+                cum = !cum;
+            }
+        }
+
+        void CPUKILL_9()
+        {
+            while (true)
+            {
+                cum = !cum;
+            }
+        }
+
+        void CPUKILL_10()
+        {
+            while (true)
+            {
+                cum = !cum;
+            }
+        }
+
+        void CPUKILL_11()
+        {
+            while (true)
+            {
+                cum = !cum;
+            }
+        }
+
+        void CPUKILL_12()
+        {
+            while (true)
+            {
+                cum = !cum;
+            }
+        }
+
+        void CPUKILL_13()
+        {
+            while (true)
+            {
+                cum = !cum;
+            }
+        }
+
+        void CPUKILL_14()
+        {
+            while (true)
+            {
+                cum = !cum;
+            }
+        }
+
+        void CPUKILL_15()
+        {
+            while (true)
+            {
+                cum = !cum;
+            }
+        }
+
+        void CPUKILL_16()
+        {
+            while (true)
+            {
+                cum = !cum;
+            }
+        }
+
+        void CPUKILL_17()
+        {
+            while (true)
+            {
+                cum = !cum;
+            }
+        }
+
+        void CPUKILL_18()
+        {
+            while (true)
+            {
+                cum = !cum;
+            }
+        }
+
+        void CPUKILL_19()
+        {
+            while (true)
+            {
+                cum = !cum;
+            }
+        }
+
+        void CPUKILL_20()
+        {
+            while (true)
+            {
+                cum = !cum;
+            }
+        }
+
+        void CPUKILL_21()
+        {
+            while (true)
+            {
+                cum = !cum;
+            }
+        }
+
+        void CPUKILL_22()
+        {
+            while (true)
+            {
+                cum = !cum;
+            }
+        }
+
+        void CPUKILL_23()
+        {
+            while (true)
+            {
+                cum = !cum;
+            }
+        }
+  }
